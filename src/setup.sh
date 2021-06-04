2ab378807b6b67c9696ceab102ca213907438b9c
r-a-17 <56036284+r-a-17@users.noreply.github.com>
2021-06-05 01:06:00 +0530

    Update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -2,10 +2,13 @@ pip3 install --upgrade pip
 python3 -m pip install --upgrade setuptools
 sudo apt install libasound2-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg
 sudo apt install python3-venv
+!pip3 uninstall keras-nightly
+!pip3 uninstall -y tensorflow
 pip3 install librosa
 pip3 install matplotlib
-pip3 install h5py==2.10.0
-%tensorflow_version 1.x
+!pip3 install keras==2.1.6
+!pip3 install tensorflow==1.15.0
+!pip3 install h5py==2.10.0
 sudo apt-get install -y python3-pyaudio
 pip3 install keras==2.3.1
 pip3 install torch==1.5.0
